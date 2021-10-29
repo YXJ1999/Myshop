d7e4c1828c702f4531045cb7232e717498118c59
YXJ1999 <996832282@qq.com>
2021-10-29 16:50:04 +0800

    2021.10.29忘了改了啥回去在添一点

--- myshop/src/main/java/com/yang/myshop/service/imp/UserSerivceImp.java
+++ myshop/src/main/java/com/yang/myshop/service/imp/UserSerivceImp.java
@@ -5,8 +5,10 @@ import com.yang.myshop.pojo.User;
 import com.yang.myshop.service.UserSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class UserSerivceImp implements UserSerivce {
     @Autowired
     private UserDao userDao;
